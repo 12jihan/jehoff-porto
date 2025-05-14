d14b3f2cfb98835c8bd2d5344cae6e3508a5dce3
12jihan <jareem.e.hoff@gmail.com>
2025-05-14 14:10:42 -0400

    Finished off a rudimentary version of the Resume page so I should be good to push through to the other pages

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -2,38 +2,39 @@ import "./Resume.scss";
 import experience from "../../experience";
 import { ReactElement } from "react";
 import { Job } from "../../interfaces/Jobs/Job";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileCode } from "@fortawesome/free-regular-svg-icons";
 
 function Resume(): ReactElement {
-  function limitString(text: string, maxLength: number): string {
-    let shortened: string = "";
-    if (text.length >= maxLength) {
-      shortened = text.substring(0, maxLength) + " " + "...";
-    } else {
-      shortened = text;
-    }
-    return shortened;
-  }
+  // function limitString(text: string, maxLength: number): string {
+  //   let shortened: string = "";
+  //   if (text.length >= maxLength) {
+  //     shortened = text.substring(0, maxLength) + " " + "...";
+  //   } else {
+  //     shortened = text;
+  //   }
+  //   return shortened;
+  // }
 
   return (
     <>
-      <div className="header-container">
-        <h2>Resume</h2>
-      </div>
-      <section className="container">
+      <section className="resume-container">
+        <div className="header-container">
+          <h2>Resume</h2>
+        </div>
         <div className="job-container">
           {experience.map((job: Job): ReactElement => {
             return (
               <>
                 <div className="job-wrapper">
                   <div className="job-header-group">
-                    <span className="job-icon">
-                      <FontAwesomeIcon icon={faFileCode} />
-                    </span>
-                    <h3 className="job-header">{job.name}</h3>
+                    <p className="job-item">
+                      <span className="job-name">{job.name}</span>
+                    </p>
+                    <p className="job-item">{job.dates}</p>
+                    <p className="job-item">
+                      <i>{job.location}</i>
+                    </p>
                   </div>
-                  <ul className="job-description">
+                  <ul className="job-body">
                     {job.description.map((desc: string): ReactElement => {
                       return <li>{desc}</li>;
                     })}
@@ -44,43 +45,6 @@ function Resume(): ReactElement {
           })}
         </div>
       </section>
-      {/* <section className="container jobs-wrapper"> */}
-      {/*   {experience.map((job: Job, index: number): ReactElement => { */}
-      {/*     return ( */}
-      {/*       <> */}
-      {/*         <div className="__long-card" key={index}> */}
-      {/*           <div className="__long-card-header"> */}
-      {/*             <p className="job-name">{job.name}</p> */}
-      {/*             <p className="job-title">{job.title}</p> */}
-      {/*             <p className="job-location">New York, NY (Remote)</p> */}
-      {/*             <p className="job-date">{job.dates}</p> */}
-      {/*           </div> */}
-      {/*           <div className="__long-card-body"> */}
-      {/*             <ul> */}
-      {/*               {job.description */}
-      {/*                 .slice(0, 3) */}
-      {/*                 .map((bullet: string): ReactElement => { */}
-      {/*                   return <li key={bullet}>{limitString(bullet, 100)}</li>; */}
-      {/*                 })} */}
-      {/*             </ul> */}
-      {/**/}
-      {/*             <div className="badge-group-h"> */}
-      {/*               {job.tech.map( */}
-      {/*                 (tech: string, index: number): ReactElement | null => { */}
-      {/*                   return ( */}
-      {/*                     <span className="badge" key={index}> */}
-      {/*                       {tech} */}
-      {/*                     </span> */}
-      {/*                   ); */}
-      {/*                 }, */}
-      {/*               )} */}
-      {/*             </div> */}
-      {/*           </div> */}
-      {/*         </div> */}
-      {/*       </> */}
-      {/*     ); */}
-      {/*   })} */}
-      {/* </section> */}
     </>
   );
 }
