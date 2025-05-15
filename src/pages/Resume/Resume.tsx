2e19cc09c98e7916d9bae5f71a4bab310717e09b
12jihan <jareem.e.hoff@gmail.com>
2025-05-14 20:15:42 -0400

    made all the necessary changes to get me setup to make large changes later

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -2,47 +2,76 @@ import "./Resume.scss";
 import experience from "../../experience";
 import { ReactElement } from "react";
 import { Job } from "../../interfaces/Jobs/Job";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import { faArrowDownShortWide } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDownLong } from "@fortawesome/free-solid-svg-icons/faArrowDownLong";
 
 function Resume(): ReactElement {
-  // function limitString(text: string, maxLength: number): string {
-  //   let shortened: string = "";
-  //   if (text.length >= maxLength) {
-  //     shortened = text.substring(0, maxLength) + " " + "...";
-  //   } else {
-  //     shortened = text;
-  //   }
-  //   return shortened;
-  // }
-
   return (
     <>
       <section className="resume-container">
         <div className="header-container">
           <h2>Resume</h2>
         </div>
-        <div className="job-container">
-          {experience.map((job: Job): ReactElement => {
-            return (
-              <>
-                <div className="job-wrapper">
-                  <div className="job-header-group">
-                    <p className="job-item">
-                      <span className="job-name">{job.name}</span>
-                    </p>
-                    <p className="job-item">{job.dates}</p>
-                    <p className="job-item">
-                      <i>{job.location}</i>
-                    </p>
-                  </div>
-                  <ul className="job-body">
-                    {job.description.map((desc: string): ReactElement => {
-                      return <li>{desc}</li>;
-                    })}
-                  </ul>
-                </div>
-              </>
-            );
-          })}
+        <div className="con-container">
+          <div className="jobs-header">
+            <div className="header-item job-status">
+              <p>
+                <b>WORK STATUS:</b> <u>AVAILABLE</u>
+              </p>
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                color="#87cf55"
+                enableBackground="true"
+                size="1x"
+              />
+            </div>
+            <button type="button" className="header-item job-download-resume">
+              Download Resume
+              <FontAwesomeIcon icon={faArrowDownLong} />
+            </button>
+          </div>
+          <div className="job-container">
+            <div className="jobs-wrapper">
+              {experience.map((job: Job): ReactElement => {
+                return (
+                  <>
+                    <div className="job-wrapper">
+                      <div className="job-header-group">
+                        <p className="job-item">
+                          <span className="job-name">{job.name}</span>
+                        </p>
+                        <p className="job-item">{job.dates}</p>
+                        <p className="job-item">
+                          <i>{job.location}</i>
+                        </p>
+                      </div>
+                      <ul className="job-body">
+                        {job.description.map((desc: string): ReactElement => {
+                          return <li>{desc}</li>;
+                        })}
+                      </ul>
+                      <div className="badge-group-h">
+                        {job.tech.map(
+                          (
+                            tech: string,
+                            index: number,
+                          ): ReactElement | null => {
+                            return (
+                              <span className="badge" key={index}>
+                                {tech}
+                              </span>
+                            );
+                          },
+                        )}
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </section>
     </>
