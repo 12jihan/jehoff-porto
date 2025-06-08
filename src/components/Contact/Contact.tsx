2de99cede37d0d38fd87053e8a0bf2c973e945c1
Jareem Hoff <jareem.e.hoff@gmail.com>
2025-06-08 12:33:35 -0400

    updated

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,14 +5,14 @@ import { NavigateFunction } from "react-router";
 
 export default function Contact(): ReactElement {
   const nav: NavigateFunction = useNavigate();
-  const goToContact = (): void => {
+  const goToContact: () => void = (): void => {
     nav("/contact");
   };
 
   return (
     <section className="contact-container">
       <h2 className="section-header-center">Let's Connect!</h2>
-      <div className="section-body">
+      <div className="section-body-center">
         <p>
           I'm always excited to discuss new projects and opportunities. Whether
           you need help with a specific challenge or want to explore potential
